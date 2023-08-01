18df3e1c72c2ce81f45f7e6e47342571af73d1b7
Inguna Preize <101196982+preizeinguuna@users.noreply.github.com>
2023-08-01 20:14:55 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
- function validateForm() {
-            var title = document.forms["myform"]["title"].value;
-            var subtitle = document.forms["myform"]["subtitle"].value;
-            var author = document.forms["myform"]["author"].value;
-            var content = document.forms["myform"]["content"].value;
+// Function to validate the form fields
+function validateForm() {
+    var title = document.forms["myform"]["title"].value;
+    var subtitle = document.forms["myform"]["subtitle"].value;
+    var author = document.forms["myform"]["author"].value;
+    var content = document.forms["myform"]["content"].value;
 
-            if (title === "" || subtitle === "" || author === "" || content === "") {
-                alert("All fields are mandatory. Please fill in all required fields.");
-                return false;
-            }
-        }
+    if (title === "" || subtitle === "" || author === "" || content === "") {
+        alert("All fields are mandatory. Please fill in all required fields.");
+        return false;
+    }
+
+    // Additional validation logic if needed...
+
+    // Form submission will proceed if all validations pass
+    return true;
+}
